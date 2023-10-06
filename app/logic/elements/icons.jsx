648dbc1686414f3561dbe45eb672626a56d1b71a
rortegaez <raulor83@gmail.com>
2023-10-06 15:30:24 +0200

    reorganizo files

---
+++ app/logic/elements/icons.jsx
@@ -0,0 +1,37 @@
+export const icons = [
+  {
+    name: "html5",
+    img: "/icons/html240.png",
+    alt: "icono html 5",
+  },
+  {
+    name: "css3",
+    img: "/icons/css3-240.png",
+    alt: "icono css3",
+  },
+  {
+    name: "javascript",
+    img: "/icons/javascript240.png",
+    alt: "icono javascript",
+  },
+  {
+    name: "react",
+    img: "/icons/react80.png",
+    alt: "icono react",
+  },
+  {
+    name: "node",
+    img: "/icons/nodejs240.png",
+    alt: "icono node",
+  },
+  {
+    name: "nextjs",
+    img: "/icons/nextjs240.png",
+    alt: "icono next js",
+  },
+  {
+    name: "git",
+    img: "/icons/git.png",
+    alto: "icono git",
+  },
+];
--- app/logic/elements/projects.jsx
+++ app/logic/elements/projects.jsx
@@ -2,13 +2,19 @@ export const projects = [
   {
     name: "PlayPokemon",
     url: "https://reto-pokemon-ashy.vercel.app/",
-    img: "/capturaPlayPokemon.png",
+    img: "/capturas/playPokemon.png",
     alt: "Web Play Pokemon",
   },
   {
     name: "TodoApp",
     url: "https://todo-front-chi.vercel.app/",
-    img: "/capturaTodoApp.png",
+    img: "/capturas/todoApp.png",
     alt: "Web Todo App",
+  },
+  {
+    name: "Ampa Fernando Magallanes",
+    url: "https://ampafront.vercel.app/",
+    img: "/capturas/ampa.png",
+    alt: "Web Ampa Fernando Magallanes",
   },
 ];
